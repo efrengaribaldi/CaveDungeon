a4e27c0e15a04d2a3973762412b0d30f2e19919a
Lars17S <efren.garibaldi@gmail.com>
2019-02-27 16:13:20 -0600

    Inventory methods

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,11 @@
 package src.game;
 
+import src.item.*;
+
 public class Main {
     public static void main(String[] args) {
         Game newGame = new Game();
+        Inventory newInventory = new Inventory();
+        System.out.println(newInventory.inventoryToString());
     }
 }
--- src/item/Inventory.java
+++ src/item/Inventory.java
@@ -7,17 +7,33 @@ import src.item.potion.*;
 import src.item.weapon.*;
 
 public class Inventory {
-    public Item[] armor = new Armor[4];
-    public Item[] chest = new Chest[3];
-    public Item[] key = new Key[2];
-    public Item[] potion = new Potion[4];
-    public Item[] weapons = new Weapon[2];
+    public Item[][] items;
+
+    public Inventory() {
+      items = new Item[5][]; // Five classes for Items
+      items[0] = new Armor[4];
+      items[1] = new Chest[3];
+      items[2] = new Key[2];
+      items[3] = new Potion[4];
+      items[4] = new Weapon[2];
+
+    }
 
     public void addItemInventory(Item item, int index) {
-
     }
 
     public void removeItemInventory(int index) {
 
+    }
+
+    public String inventoryToString() {
+        String inventoryText = "";
+        for(int i = 0; i < items.length; ++i) {
+            for(int j = 0; j < items[i].length; ++j) {
+                inventoryText += "Item: " + items[i][j] + " ";
+            }
+            inventoryText += "\n";
+        }
+        return inventoryText;
     }
 }
