04b45188e5ed3f7420a9eedcea936ade398a35c9
MarioJim <mario.emilio.j@gmail.com>
2019-03-22 09:41:20 -0600

    Moved Floor to Tile, deleted Wall and Door

--- tests/map/Tile.java
+++ tests/map/Tile.java
@@ -1,12 +1,85 @@
 package tests.map;
 
-public class Tile {
-    public Tile() {
+import tests.map.Tile;
+import tests.character.npc.Enemy;
+import tests.character.npc.enemy.*;
+import tests.character.npc.Boss;
+import tests.character.npc.boss.*;
+import tests.item.misc.Chest;
 
+public class Tile {
+    private int spriteNum;
+    private Enemy enemy;
+    private Boss boss;
+    private Chest chest;
+    private boolean skull;
+
+    public Tile() {
+        super();
+        // 4 in a 1000 chance it as a skull
+        skull = (Math.random() < 0.004);
+        int rand = (int) (Math.random() * 20) - 13;
+        spriteNum = (rand < 1) ? 1 : rand;
     }
 
-    // Methods overriden by wall
-    public int getSide() {
-        return -1;
+    public void addEnemy() {
+        int randomEnemy = (int) (Math.random() * 5);
+        switch (randomEnemy) {
+        case 0:
+            enemy = new Chort();
+            break;
+        case 1:
+            enemy = new Necromancer();
+            break;
+        case 2:
+            enemy = new Skeleton();
+            break;
+        case 3:
+            enemy = new Swampy();
+            break;
+        case 4:
+            enemy = new Zombie();
+            break;
+        }
+    }
+
+    public boolean hasEnemy() {
+        return enemy != null || boss != null;
+    }
+
+    public void addBoss() {
+        skull = false;
+        int randomBoss = (int) (Math.random() * 4);
+        switch (randomBoss) {
+        case 0:
+            boss = new BigDemon();
+            break;
+        case 1:
+            boss = new BigZombie();
+            break;
+        case 2:
+            boss = new Ogre();
+            break;
+        case 3:
+            boss = new Wizzard();
+            break;
+        }
+    }
+
+    public void addChest() {
+        // Add random items when generating the chest
+        chest = new Chest();
+    }
+
+    public boolean hasChest() {
+        return chest != null;
+    }
+
+    public boolean hasSkull() {
+        return skull;
+    }
+
+    public String getSpritePath() {
+        return "./tiles/imgFloor/" + spriteNum + ".png";
     }
 }
